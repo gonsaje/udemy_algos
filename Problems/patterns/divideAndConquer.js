115e460b190e0dbaf94da6d30182820f3fdcf386
gonsaje <song.jaehyun@gmail.com>
2021-02-11 13:30:35 -0500

    finished divide and conquer / added practice problems

--- Problems/patterns/divideAndConquer.js
+++ Problems/patterns/divideAndConquer.js
@@ -1,1 +1,46 @@
 // DIVIDE AND CONQUER
+
+// This pattern invlives dividng a data set into smaller chunks
+// and then repeating a process with a subset of data
+
+// THis pattern can tremendously decrease time complexity
+
+// Given a sorted array of integers, write a function
+// called search, that accepts a value and returns the index
+// where the value is passed to the function is location.
+// If the falue is not found return -1
+
+// linear search (NON DIVIDE AND CONQUER)
+
+function search(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) return i;
+    }
+    return -1;
+}
+
+// binary search
+// Time Olog(N) => we half our search by half each iteration
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let ele = nums[mid];
+
+        if (ele < val) {
+            min = mid + 1;
+        } else if (ele > val) {
+            max = mid - 1;
+        } else {
+            return mid;
+        }
+    }   
+
+    return -1;
+}
+
+console.log(search([1,2,3,4,5,6], 4)) // 3
+console.log(search([1,2,3,4,5,6], 6)) // 5
+console.log(search([1,2,3,4,5,6], 11)) // -1
---
+++ Problems/practice/solutions.js
@@ -0,0 +1,84 @@
+// Frequency Counter
+/************************************************************
+Write a function sameFrequency(num1, num2). Given two positive
+integers, find out if the two numbers have the same frequency of digits.
+
+Constraint: Solution must be in O(N) time complexity.
+
+sameFrequency(182, 281) => true
+sameFrequency(34, 14) => false
+sameFrequency(3589578, 5879385) => true
+sameFrequency(22, 222) => false
+*************************************************************/
+// time O(N) | space O(N)
+function sameFrequency(num1, num2) {
+    if (String(num1).length !== String(num2).length) return false;
+
+    let freq1 = digitFreq(num1); // O(N) time
+    let freq2 = digitFreq(num2); // O(N) time
+
+    for (let num in freq1) { // O(N) time
+        if (freq1[num] !== freq2[num]) return false;
+    }
+
+    return true;
+}
+
+function digitFreq(num) { //function bigO: O(N) space & time
+    let count = {}; // O(N) space
+    for (let digit of String(num)) { // O(N) time;
+        count[digit] === undefined ? count[digit] = 1 : count[digit]++;
+    }
+    return count;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Frequency Counter/Multiple Pointers
+/************************************************************
+Write a function areThereDuplicates(...args) which accepts a
+variable number of arguments, and checks wheter ther are any 
+duplicates among the argumenbts passed in.
+You can solve this using the frequency counter pattern OR
+the multiple pointers pattern.
+
+Constraint: Solution must be in O(N) time and space complexity.
+Bonus: time O(n log n) | space O(1)
+
+areThereDuplicates(1, 2, 3) => false;
+areThereDuplicates(1, 2, 2) => true;
+areThereDuplicates('a', 'b', 'c', 'a') => true;
+*************************************************************/
+
+// Frequency Counter Approach
+function areThereDuplicates(...args) { // O(N) space | O(N) time
+    let count = {}; // O(N) space
+
+    for (let ele of args) { //O(N) time
+        count[ele] ? count[ele]++ : count[ele] = 1; // O(1) time
+    }
+
+    for (let key in count) { // O(N) time
+        if (count[key] > 1) return true; // O(1) time
+    }
+
+    return false;
+}
+
+// Multiple Pointers Approach
+
+function areThereDuplicates(...args) {
+    
+    for (let i = 0; i < args.length; i++) {
+        for (let j = i + 1; j < args.length; j++) {
+            if (args[i] === args[j]) return true;
+        }
+    }
+
+    return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
