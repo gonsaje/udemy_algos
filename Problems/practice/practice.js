ffd7d8505e4e33a11c40fb6f44f5650b1f5de78f
gonsaje <song.jaehyun@gmail.com>
2021-03-23 14:35:03 -0400

    recursion init

--- Problems/practice/practice.js
+++ Problems/practice/practice.js
@@ -83,6 +83,84 @@ function areThereDuplicates(...args) {
     return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3) )//=> false;
-console.log(areThereDuplicates(1, 2, 2))// => true;
-console.log(areThereDuplicates('a', 'b', 'c', 'a'))// => true;
+// console.log(areThereDuplicates(1, 2, 3) )//=> false;
+// console.log(areThereDuplicates(1, 2, 2))// => true;
+// console.log(areThereDuplicates('a', 'b', 'c', 'a'))// => true;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Multiple Pointer - averagePair
+
+/****************************************************************************
+Write a function called averagePair(arr, avg). Given a sorted array of integers
+and a target average, determine if there is a pair of values in the array where
+the average of the pair equals the target average. There may be more than one pair
+that matches the target average.
+
+BONUS CONSTRAINTS:
+time O(N) & space O(1)
+
+averagePair([1,2,3], 2.5) //true
+averagePair([1,3,3,5,6,7,10,12,19], 8) //true
+averagePair([-1,0,3,4,5,6], 4.1) //false
+averagePair([], 4) //false
+*****************************************************************************/
+
+// Approach:
+// have pointer i & j
+
+// first attempt
+function averagePair(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if ((arr[i] + arr[j]) / 2 === target) return true;
+        }
+    }
+    return false;
+}
+
+// refactored solution
+function averagePair(arr, target) {
+    let i = 0; // O(1) space
+    let j = arr.length - 1; // O(1) space
+
+    while (i < j) { // O(N)
+        let avg = (arr[i] + arr[j]) / 2; // O(1) space and time
+
+        if (avg < target) i++; // O(1)
+        else if (avg > target) j--; // O(1)
+        else return true;
+    }
+
+    return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Multiple Pointer - isSubsequence
+
+/****************************************************************************
+Write a function isSubsequence which takes in 2 strings and checks whether
+the characters in the first string form a subsequence of the characters in 
+the second string. In other words, the function should check whether the 
+characters in the first string appear somewhere in the second string,
+WITHOUT THEIR ORDER CHANGING.
+
+Solution must AT LEAST have these constraints:
+time O(N + M) & space O(1)
+
+isSubsequence('hello', 'hello world') // true
+isSubsequence('sing', 'sting') // true
+isSubsequence('abc', 'abracadabra') // true
+isSubsequence('abc', ) // true
+*****************************************************************************/
+function isSubsequence(str1, str2) {
+    for (let i = 0; i < str1.length; i++) { // O(N)
+
+    }
+    
+    for (let i = 0; i < str2.length; i++) { // O(M)
+
+    }
+}
--- Problems/practice/solutions.js
+++ Problems/practice/solutions.js
@@ -70,15 +70,55 @@ function areThereDuplicates(...args) { // O(N) space | O(N) time
 // Multiple Pointers Approach
 
 function areThereDuplicates(...args) {
-    
-    for (let i = 0; i < args.length; i++) {
-        for (let j = i + 1; j < args.length; j++) {
-            if (args[i] === args[j]) return true;
-        }
+    args.sort((a,b) => a-b); //O(nlogn)
+    let start = 0;
+    let next = 1;
+    while (next < args.length) {
+        if (args[start] === args[next]) return true;
+        start++;
+        next++;
     }
 
     return false;
 }
 
 
+// One Liner Solution
+function areThereDuplicates() {
+    return new Set(arguments).size !== arguments.length;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// averagePair Solution
+function averagePair(arr, num){
+  let start = 0
+  let end = arr.length-1;
+  while(start < end){
+    let avg = (arr[start]+arr[end]) / 2 
+    if(avg === num) return true;
+    else if(avg < num) start++
+    else end--
+  }
+  return false;
+}
+
+// isSubsequence Solution - Iterative
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// isSubsequence Solution - Recursive but not O(1) Space
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}
