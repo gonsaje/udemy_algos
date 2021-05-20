b0fe89a9f2059d1a0c39b878cb54c028b2accb69
gonsaje <song.jaehyun@gmail.com>
2021-05-20 19:00:48 -0400

    init singly linked list

--- Data Structures/singlylinkedList.js
+++ Data Structures/singlylinkedList.js
@@ -1,4 +1,17 @@
+/* 
+Define Singly Linked List
+Compare and Contrast with Arrays and LinkedLists
+
+Implement insertion,removal
+\*/
 
 
+// LinkedList: contains a head, tail, and length property
 
-function targetSum(arr1, arr2, target)
+// Linked Lists consist of nodes and each node has a value and a pointer to another node or null
+
+// head                 tail
+// [4] => [6] => [8] => [2] => null
+// length = 4
+
+// in order to get to a certain Node, you have to start from head and keep asking for the next Node.
