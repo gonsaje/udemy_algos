21e15484abf23dbd3b3c396109e34566094f50b4
gonsaje <song.jaehyun@gmail.com>
2021-05-20 16:15:13 -0400

    overview for data structures

---
+++ Data Structures/dataStructures.js
@@ -0,0 +1,22 @@
+// What do they do?
+
+// Collections of values, the relationships among them, 
+// and the functions or operations that can be applied to the data
+
+// Different data structures scel at different things. Some are highly
+// specialized, while others (like arrays) are more generally used.
+
+
+// The more tim you spend as a dev, the more likely you need to ue one of these data structures
+
+// Working with map/location data? Use a graph
+
+// Need an ordered list with fast inserts/removals at the beginning and the end? Use a linked list
+
+// Web scraping nested HTML? Use a tree!
+
+// Need to write a scheduler? use a binary heap
+
+
+
+
